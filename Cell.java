ca60d3e9c6db1d960e928acd50e1b9034b97725c
Isuru Shamal Dilanga Dissanayake <67257017+IsuruShamalDIlangaDissanayake@users.noreply.github.com>
2020-07-01 14:05:18 +0530

    Delete Cell.java

--- Cell.java
+++
@@ -1,9 +0,0 @@
-package Assignment;
-
-public class Cell {
-	
-	public char content;  // The character in this cell.
-	public Cell next;     // Pointer to the cell to the right of this one.
-	public Cell prev; 
-
-}
